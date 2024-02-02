105d9936c09fcf1986f4d7072f6fb0cee45c2793
oscaralfrm <oscaralfrm1996@gmail.com>
2024-02-01 22:24:06 -0300

    Agregando BBDD

---
+++ concesionariaAutosDB/concesionariaautos_automovil.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: concesionariaautos
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `automovil`
+--
+
+DROP TABLE IF EXISTS `automovil`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `automovil` (
+  `AUTOID` int NOT NULL AUTO_INCREMENT,
+  `CANTIDADPUERTAS` int DEFAULT NULL,
+  `COLOR` varchar(255) DEFAULT NULL,
+  `MARCA` varchar(255) DEFAULT NULL,
+  `MODELO` varchar(255) DEFAULT NULL,
+  `MOTOR` varchar(255) DEFAULT NULL,
+  `PATENTE` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`AUTOID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `automovil`
+--
+
+LOCK TABLES `automovil` WRITE;
+/*!40000 ALTER TABLE `automovil` DISABLE KEYS */;
+INSERT INTO `automovil` VALUES (1,2,'Plateado','DeLorean Motor Company','DeLorean','DeLorean','XYZ123'),(2,3,'Plateado','Toyota','Yaris','Yaris','VBV234'),(4,4,'Azul','Honda','X300','X300','ABX123');
+/*!40000 ALTER TABLE `automovil` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-01 21:57:14
